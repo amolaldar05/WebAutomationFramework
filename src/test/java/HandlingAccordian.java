ccb281494c5dd9aa879eca8385f860c1f6d5b0c3
Amol Aldar <amolaldar05@gmail.com>
2024-10-26 22:30:33 +0400

    pagination

--- src/test/java/HandlingAccordian.java
+++ src/test/java/HandlingAccordian.java
@@ -3,36 +3,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 import java.util.List;
 
 public class HandlingAccordian {
 
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://stg.damacproperties.com/en-gb/projects/damac-tower-nine-elms-london/for-sale/properties/2-apartment/");
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get(
+				"https://stg.damacproperties.com/en-gb/projects/damac-tower-nine-elms-london/for-sale/properties/2-apartment/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		// Click on 'Select Unit' button
+		driver.findElement(By.xpath("//button[text()='Select Unit']")).click();
 
-        // Click on 'Select Unit' button
-        driver.findElement(By.xpath("//button[text()='Select Unit']")).click();
+		// Locate the list of available units
+		List<WebElement> availableUnitsTitles = driver
+				.findElements(By.cssSelector("div.relative div.hidden.md\\:block div[class*='grid units-grid']"));
 
-        // Locate the list of available units
-        List<WebElement> availableUnitsTitles = driver.findElements(By.cssSelector("div.relative div.hidden.md\\:block div[class*='grid units-grid']"));
+		// Loop through the list to find the desired Unit ID
+		for (int i = 0; i < availableUnitsTitles.size(); i++) {
+			WebElement availableUnitsTitle = availableUnitsTitles.get(i);
 
-        // Loop through the list to find the desired Unit ID
-        for (int i = 0; i < availableUnitsTitles.size(); i++) {
-            WebElement availableUnitsTitle = availableUnitsTitles.get(i);
+			// Check if the unit ID matches the desired one
+			if (availableUnitsTitle.getText().equalsIgnoreCase("unit ID")) {
+				// Find the corresponding unit by index
+				WebElement availableUnit = driver.findElement(By.cssSelector(
+						"div.grid.group.units-grid > div.font-secondary.text-s:nth-child(" + (i + 1) + ")"));
+				System.out.println("Unit found: " + availableUnit.getText());
+				// Perform your actions here on the found unit...
+				break; // Exit the loop once the desired unit is found
+			}
+		}
 
-            // Check if the unit ID matches the desired one
-            if (availableUnitsTitle.getText().equalsIgnoreCase("unit ID")) {
-                // Find the corresponding unit by index
-                WebElement availableUnit = driver.findElement(By.cssSelector("div.grid.group.units-grid > div.font-secondary.text-s:nth-child(" + (i + 1) + ")"));
-                System.out.println("Unit found: " + availableUnit.getText());
-                // Perform your actions here on the found unit...
-                break; // Exit the loop once the desired unit is found
-            }
-        }
-
-        // Close the browser after operations
-        driver.quit();
-    }
+		// Close the browser after operations
+		driver.quit();
+	}
 }
--- src/test/java/TestClasses/HandlingAccordian.java
+++ src/test/java/TestClasses/HandlingAccordian.java
@@ -8,49 +8,77 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HandlingAccordian {
 
-	@Test
-	public void test() throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get(
-				"https://stg.damacproperties.com/en-gb/projects/damac-tower-nine-elms-london/for-sale/properties/2-apartment/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    WebDriver driver;
 
-		// Click on 'Select Unit' button
-		driver.findElement(By.xpath("//button[text()='Select Unit']")).click();
+    @BeforeTest
+    public void presetup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://stg.damacproperties.com/en-gb/projects/damac-tower-nine-elms-london/for-sale/properties/2-apartment/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
 
-		// Locate the list of available units
-		List<WebElement> availableUnitsTitles = driver
-				.findElements(By.cssSelector("div.relative div.hidden.md\\:block div[class*='grid units-grid'] div"));
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].scrollIntoView(true);", availableUnitsTitles.get(0));
-		Thread.sleep(2000);
-		for (int i = 0; i < availableUnitsTitles.size(); i++) {
-			if (availableUnitsTitles.get(i).getText().equalsIgnoreCase("unit id")) {
-				List<WebElement> availableUnits = driver.findElements(By.cssSelector(
-						"div.grid.group.units-grid > div.font-secondary.text-s:nth-child(" + (i + 1) + ")"));
+    @Test(priority = 0)
+    public void test() throws InterruptedException {
+        // Click on 'Select Unit' button
+        driver.findElement(By.xpath("//button[text()='Select Unit']")).click();
 
-				availableUnits.stream().map(WebElement::getText).forEach(System.out::println);
-				for (int j = 0; j < availableUnits.size(); j++) {
+        // Locate the list of available units
+        List<WebElement> availableUnitsTitles = driver.findElements(By.cssSelector(
+                "div.relative div.hidden.md\\:block div[class*='grid units-grid'] div"));
 
-					if (availableUnits.get(j).getText().equalsIgnoreCase("AYK/5/501")) {
-						availableUnits.get(i).click();
-						driver.findElements(By.xpath("//button[text()='Reserve now']")).get(i + 1).click();
-					}
-				}
+        // Iterate through the available units to find the one with the specified size
+        for (int i = 0; i < availableUnitsTitles.size(); i++) {
+            if (availableUnitsTitles.get(i).getText().equalsIgnoreCase("unit id")) {
+                // Locate the corresponding unit elements
+                List<WebElement> availableUnits = driver.findElements(By.cssSelector(
+                        "div.grid.group.units-grid > div.font-secondary.text-s:nth-child(" + (i + 1) + ")"));
 
-				
-			}
-		}
+                // Print all available unit texts
+                availableUnits.stream().map(WebElement::getText).forEach(System.out::println);
 
-		driver.quit();
+                // Find and click the specific unit
+                availableUnits.stream()
+                    .filter(availableUnit -> availableUnit.getText().equalsIgnoreCase("AYK/4/408"))
+                    .findFirst()
+                    .ifPresent(unit -> handleUnitClick(unit, availableUnits));
+            }
+        }
+    }
 
-	}
+    private void handleUnitClick(WebElement unit, List<WebElement> availableUnits) {
+        // Click the unit to make the "Reserve now" button visible
+        unit.click();
+        int index = availableUnits.indexOf(unit);
+
+        // Locate the "Reserve now" button that becomes visible after clicking the unit
+        WebElement reserveButton = driver.findElements(By.xpath("//button[text()='Reserve now']")).get(index + 1);
+
+        // Scroll to the "Reserve now" button
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", reserveButton);
+
+        // Click the "Reserve now" button if it's displayed
+        if (reserveButton.isDisplayed()) {
+            reserveButton.click();
+        }
+    }
+
+    @Test(priority = 1, enabled = false)
+    public void goToPropType() {
+        // Placeholder for another test method
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
